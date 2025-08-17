b41be1d95335dd9c4d02c8016dc8b7cd44c96fa2
Hugo Rucchetto <hugorucchetto@hotmail.com>
2025-08-17 08:24:41 +0000

    refactoring + snapshot

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,9 +1,13 @@
 with customers as (
     select
-        id as customer_id,
-        first_name,
-        last_name
-    from {{ source('jaffle_shop', 'customers') }}
+        id as customer_id
+        , first_name
+        , last_name
+    from 
+        {{ source('jaffle_shop', 'customers') }}
 )
 
-select * from customers
+select 
+    * 
+from 
+    customers
